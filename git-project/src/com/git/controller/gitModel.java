09e3437ee569bb628fb317608e3ac84d228ef798
wuyouyou2020 <1783665786@qq.com>
2020-06-11 09:11:17 +0800

    提交到本地仓库

---
+++ git-project/src/com/git/controller/gitModel.java
@@ -0,0 +1,17 @@
+package com.git.controller;
+
+public class gitModel {
+  private int id;
+  private String username;
+  private String password;
+  private int sex;
+  private String love;
+  public gitModel(int id,String username,String password,int sex,String love) {
+	  super();
+	 this.id=id;
+	 this.username=username;
+	 this.password=password;
+	 this.sex=sex;
+	 this.love=love;
+  }
+}
